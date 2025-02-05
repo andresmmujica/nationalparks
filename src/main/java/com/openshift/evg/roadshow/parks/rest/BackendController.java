5f090353a6accb0b3d0410a1dab32daca056aa1d
Andrés Mauricio Mujica Zalamea <andres.mujica@seaq.co>
2025-02-05 10:00:36 -0500

    Update BackendController.java

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,6 +17,6 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Backend get() {
-        return new Backend("nationalparks","Andres National Parks", new Coordinates("47.039304", "14.505178"), 4);
+        return new Backend("nationalparks","Github Actions National Parks", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
