ad1af1af3fe16a51075089994d13b163c54c5cf7
Andrés Mauricio Mujica Zalamea <andres.mujica@seaq.co>
2025-02-13 13:01:55 +0000

    Update BackendController.java GitHub Action the Real One

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,6 +17,6 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Backend get() {
-        return new Backend("nationalparks","GitHub Action by Andres National Parks", new Coordinates("47.039304", "14.505178"), 4);
+        return new Backend("nationalparks","GitHub Action the Real One by Andres National Parks", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
