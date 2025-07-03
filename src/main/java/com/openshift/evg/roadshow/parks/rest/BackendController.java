6cde157f7155da9c276cb449c5b5cc2d9d5dda55
Andrés Mauricio Mujica Zalamea <andres.mujica@seaq.co>
2025-07-02 21:29:42 -0500

    Update BackendController.java with GitHub Actions

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,6 +17,6 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Backend get() {
-        return new Backend("nationalparks","GitHub WebHook Modification - V2 - Andres", new Coordinates("47.039304", "14.505178"), 4);
+        return new Backend("nationalparks","GitHub Actions - Andres", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
